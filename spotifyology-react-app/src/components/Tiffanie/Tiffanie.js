795863611fbfea1e79ba4faf1fd4ccbd37a33319
Tiffanie <tiffanielchau@gmail.com>
2021-04-04 15:56:13 -0700

    first error

--- spotifyology-react-app/src/components/Tiffanie/Tiffanie.js
+++ spotifyology-react-app/src/components/Tiffanie/Tiffanie.js
@@ -1,21 +1,26 @@
 import React, { useRef } from 'react';
 import { NavLink } from 'react-router-dom';
+import playlistSongComponent from "./tiff_playlist"
 import { css } from 'emotion';
 
 
 
 class Tiffanie extends React.Component {
     constructor(props) {
-        super(props); 
+        super(props);
     }
 
     render() {
         return (
         <body>
-            {/* Your component here */}
+            <playlistSongComponent
+              letter= "I"
+              title= "If the World was Ending"
+              artist= "JP Saxe, Julia Michaels"
+            />
         </body>
         );
     }
   }
-  
+
   export default Tiffanie;
