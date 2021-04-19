1768fd578db26c609a6d07eb5b66d93362ddc38a
Tiffanie <tiffanielchau@gmail.com>
2021-04-18 17:26:21 -0700

    last meting lol

---
+++ spotifyology-react-app/src/components/Tiffanie/SongComp.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { css } from 'emotion';
+import {colors} from '../../styles/index.tsx';
+
+class SongComp extends React.Component {
+
+    playlistSongComponent = css`
+    display: flex;
+    width: 60%;
+    height: 30px;
+    text-align: left;
+    vertical-align: middle;
+    align-items: right;
+    background-color: ${colors.white};
+    border-radius: 25px;
+    `;
+    songDetailsColumn =  css`
+        flex-flow: column;
+    `;
+    letter =  css`
+        width: 10%;
+        vertical-align: middle;
+        text-align: center;
+    `;
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+           letter: props.letter,
+           title: props.title,
+           artist: props.artist
+        }
+    }
+
+    render() {
+        return (
+                <div className={this.playlistSongComponent}>
+                  <div className={this.letter}>
+                    {this.state.letter}
+                  </div>
+                  <div className={this.songDetailsColmun}>
+                    {this.state.title} :: {this.state.artist}
+                  </div>
+                </div>
+             );
+    }
+}
+
+export default SongComp;
